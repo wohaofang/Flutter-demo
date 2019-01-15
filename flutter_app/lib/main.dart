cec74774b1c72368d47022c86c940e269a13d9db
wohaofang <740148050@qq.com>
2019-01-15 10:43:10 +0800

    image

--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -14,22 +14,41 @@ class MyApp extends StatelessWidget{
         ),
         body:Center(
           child:Container(
-            child: new Text(
-              'hello world,Dm',
-              textAlign: TextAlign.left,
-              // maxLines: 2,
-              // overflow: TextOverflow.fade,
-              style: TextStyle(
-                fontSize: 40.0,
-                color:Color.fromARGB(255, 255, 150, 150),
-                decoration: TextDecoration.underline,
-                decorationStyle: TextDecorationStyle.solid,
-              ),
+            // child: new Text(
+            //   'hello world,Dm',
+            //   textAlign: TextAlign.left,
+            //   // maxLines: 2,
+            //   // overflow: TextOverflow.fade,
+            //   style: TextStyle(
+            //     fontSize: 40.0,
+            //     color:Color.fromARGB(255, 255, 150, 150),
+            //     decoration: TextDecoration.underline,
+            //     decorationStyle: TextDecorationStyle.solid,
+            //   ),
+            // ),
+            child: new Image.network(
+              'http://img.mukewang.com/5c18cf540001ac8206000338-240-135.jpg',
+              scale: 2.0,
+              // fit: BoxFit.contain,
+              // color: Colors.greenAccent,
+              // colorBlendMode: BlendMode.darken,
+              repeat: ImageRepeat.repeat,
             ),
-            alignment: Alignment.center,
+            // alignment: Alignment.topLeft,
             width: 500.0,
             height: 400.0,
-            color: Colors.lightBlue,
+            // color: Colors.lightBlue,
+            padding: const EdgeInsets.fromLTRB(10.0, 30.0, 0.0, 0.0),
+            margin: const EdgeInsets.all(10.0),
+            decoration: new BoxDecoration(
+              gradient: const LinearGradient(
+                colors:[
+                  Colors.lightBlue,
+                  Colors.greenAccent,
+                  Colors.pink
+                ]
+              )
+            ),
           )
         )
       )
