8ab2bc0df21fde0e4bfc7c396c34417ebcb58603
wohaofang <740148050@qq.com>
2019-01-16 20:18:00 +0800

    feat: add some code

--- flutter_app2/lib/main.dart
+++ flutter_app2/lib/main.dart
@@ -11,7 +11,18 @@ class MyApp extends StatelessWidget {
         appBar: new AppBar(
           title: Text('haha'),
         ),
-        body: new Row(
+        body: stackWidgets()
+      ),
+    );
+  }
+}
+
+// 横向布局
+class RowWidget extends StatelessWidget{
+  @override
+
+  Widget build(BuildContext content){
+    return new Row(
           children: <Widget>[
            Expanded(child:new RaisedButton(
               onPressed:(){} ,
@@ -29,10 +40,55 @@ class MyApp extends StatelessWidget {
               child: new Text('红色'),
             ), ),
           ],
-        ),
-      ),
+        );
+  }
+}
+
+
+// 垂直布局
+class ColumnWidget extends StatelessWidget{
+  @override
+  Widget build(BuildContext content){
+    return Center(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Text('hahah'),
+          Expanded(
+            child: Text('fsdnfskjdfsdfjsdf'),
+          ),
+          Text('haha水电费水电费h'),
+        ],
+      ) 
     );
   }
 }
 
 
+// stack 布局
+class stackWidgets extends StatelessWidget{
+  @override
+
+  Widget build(BuildContext content){
+    var stack = new Stack(
+      alignment: const FractionalOffset(0.5, 0.9),
+      children: <Widget>[
+        new CircleAvatar(
+          backgroundImage: new NetworkImage('https://ss1.baidu.com/6ONXsjip0QIZ8tyhnq/it/u=40446802,874678123&fm=173&app=25&f=JPEG?w=218&h=146&s=165FA5641863070BF484A99703008093'),
+          radius: 100.0,
+        ),
+        new Container(
+          decoration: new BoxDecoration(
+            color: Colors.lightBlue,
+          ),
+          padding: EdgeInsets.all(5.0),
+          child: Text('库克 你好'),
+        )
+      ],
+    );
+    return Center(
+      child: stack,
+    );
+  }
+}
